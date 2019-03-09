d8e2a0f8ad80a2746248eeccce3b2efa148ca6de
黄浩翔 <41788764+huanghaox1212@users.noreply.github.com>
2019-03-09 16:05:45 +0800

    Update FastIO.cpp

--- FastIO.cpp
+++ FastIO.cpp
@@ -1,89 +1,79 @@
 //使用前请务必查看readme.md中的使用说明。
-#include <bits/stdc++.h>
-#define reg register
-#define fok (ch!=EOF)
-#define sep (ch==' '||ch=='\n'||ch=='\t')
-#define dsep !isdigit(ch)
-#define neq(a,b) ((a)-(b)>1e-6)
+#include<bits/stdc++.h>
 using namespace std;
-class FastIO{
-    char rbuf[1000002],wbuf[1000002],b,*p1,*p2;
-    int rs,ws,S;
-    public:
-        FastIO():p1(rbuf),p2(wbuf),S(1000000),rs(1000000),ws(-1),b(1){}
-        ~FastIO(){fwrite(wbuf,1,ws+1,stdout);}
-        inline char getch(){
-            return rs==S&&
-            (p1=rbuf,rs=-1,(S=fread(rbuf,1,S+1,stdin)-1)==-1)?
-            (b=0,EOF):(++rs,*p1++);
+#define reg register
+struct control{
+    int ct,val;
+    control(int Ct,int Val=-1):ct(Ct),val(Val){}
+    inline control operator()(int Val){
+        return control(ct,Val);
+    }
+}_endl(0),_prs(1),_setprecision(2);
+struct FastIO{
+    #define IOSIZE 1000000
+    char in[IOSIZE],*p,*pp,out[IOSIZE],*q,*qq,ch[20],*t,b,K,prs;
+    FastIO():p(in),pp(in),q(out),qq(out+IOSIZE),t(ch),b(1),K(6){}
+    ~FastIO(){fwrite(out,1,q-out,stdout);}
+    inline char getch(){
+        return p==pp&&(pp=(p=in)+fread(in,1,IOSIZE,stdin),p==pp)?b=0,EOF:*p++;
+    }
+    inline void putch(char x){
+        q==qq&&(q=out,fwrite(out,1,q-out,stdout)),*q++=x;
+    }
+    inline void puts(const char str[]){fwrite(out,1,q-out,stdout),fwrite(str,1,strlen(str),stdout),q=out;}
+    inline void getline(string& s){
+        s="";
+        for(reg char ch;(ch=getch())!='\n'&&b;)s+=ch;
+    }
+    #define indef(T) inline FastIO& operator>>(T& x){\
+        x=0;reg char f=0,ch;\
+        while(!isdigit(ch=getch())&&b)f|=ch=='-';\
+        while(isdigit(ch))x=(x<<1)+(x<<3)+(ch^48),ch=getch();\
+        return x=f?-x:x,*this;\
+    }
+    indef(int)
+    indef(long long)
+    inline FastIO& operator>>(char& ch){return ch=getch(),*this;}
+    inline FastIO& operator>>(string& s){
+        s="";reg char ch;
+        while(isspace(ch=getch())&&b);
+        while(!isspace(ch)&&b)s+=ch,ch=getch();
+        return *this;
+    }
+    inline FastIO& operator>>(double& x){
+        x=0;reg char f=0,ch;
+        double d=0.1;
+        while(!isdigit(ch=getch())&&b)f|=(ch=='-');
+        while(isdigit(ch))x=x*10+(ch^48),ch=getch();
+        if(ch=='.')while(isdigit(ch=getch()))x+=d*(ch^48),d*=0.1;
+        return x=f?-x:x,*this;
+    }
+    #define outdef(_T) inline FastIO& operator<<(_T x){\
+        !x&&(putch('0'),0),x<0&&(putch('-'),x=-x);\
+        while(x)*t++=x%10+48,x/=10;\
+        while(t!=ch)*q++=*--t;\
+        return *this;\
+    }
+    outdef(int)
+    outdef(long long)
+    inline FastIO& operator<<(char ch){return putch(ch),*this;}
+    inline FastIO& operator<<(const char str[]){return puts(str),*this;}
+    inline FastIO& operator<<(const string& s){return puts(s.c_str()),*this;}
+    inline FastIO& operator<<(double x){
+        reg int k=0;
+        this->operator<<(int(x));
+        putch('.');
+        x-=int(x);
+        prs&&(x+=5*pow(10,-K-1));
+        while(k<K)putch(int(x*=10)^48),x-=int(x),++k;
+        return *this;
+    }
+    inline FastIO& operator<<(const control& cl){
+        switch(cl.ct){
+            case 0:putch('\n');break;
+            case 1:prs=cl.val;break;
+            case 2:K=cl.val;break;
         }
-        inline void putch(int x){
-            ws==1000000&&
-            (p2=wbuf,ws=-1,fwrite(wbuf,1,1000001,stdout)),++ws,*p2++=x;
-        }
-        inline void puts(const char str[]){
-            fwrite(wbuf,1,ws+1,stdout)?(ws=-1):0,
-            fwrite(str,1,strlen(str),stdout);
-        }
-        inline void getline(string& s){
-            for(reg char ch;(ch=getch())!='\n'&&fok;)s+=ch;
-        }
-        inline FastIO& operator>>(int& x){
-            x=0;reg char f=0,ch=getch();
-            while(dsep&&fok)f|=(ch=='-'),ch=getch();
-            while(isdigit(ch))
-                x=(x<<1)+(x<<3)+(ch^48),ch=getch();
-            return x=f?-x:x,*this;
-        }
-        inline FastIO& operator>>(long long& x){
-            x=0;reg char f=0,ch=getch();
-            while(dsep&&fok)f|=(ch=='-'),ch=getch();
-            while(isdigit(ch))
-                x=(x<<1)+(x<<3)+(ch^48),ch=getch();
-            return x=f?-x:x,*this;
-        }
-        inline FastIO& operator>>(char& ch){return ch=getch(),*this;}
-        inline FastIO& operator>>(string& s){
-            reg char ch=getch();
-            while(sep&&fok)ch=getch();
-            while(!sep&&fok)s+=ch,ch=getch();
-            return *this;
-        }
-        inline FastIO& operator>>(double& x){
-            x=0;reg char f=0,ch=getch();
-            double d=0.1;
-            while(dsep&&fok)f|=(ch=='-'),ch=getch();
-            while(isdigit(ch))x=x*10+(ch^48),ch=getch();
-            if(ch=='.')
-                while(isdigit(ch=getch()))x+=d*(ch^48),d*=0.1;
-            return x=f?-x:x,*this;
-        }
-        inline FastIO& operator<<(int x){
-            reg char ch[10],i=-1;
-            !x?(putch('0'),0):0,
-            x<0?(putch('-'),x=-x):0;
-            while(x)ch[++i]=x%10+48,x/=10;
-            while(~i)putch(ch[i]),--i;
-            return *this;
-        }
-        inline FastIO& operator<<(long long x){
-            reg char ch[20],i=-1;
-            !x?(putch('0'),0):0,
-            x<0?(putch('-'),x=-x):0;
-            while(x)ch[++i]=x%10+48,x/=10;
-            while(~i)putch(ch[i]),--i;
-            return *this;
-        }
-        inline FastIO& operator<<(char ch){return putch(ch),*this;}
-        inline FastIO& operator<<(char str[]){return puts(str),*this;}
-        inline FastIO& operator<<(const string& s){return puts(s.c_str()),*this;}
-        inline FastIO& operator<<(double x){
-            int y=int(x);
-            *this<<y;
-            x-=y;
-            while(neq(x,int(x)))x*=10;
-            x?*this<<'.'<<int(x):0;
-            return *this;
-        }
-        inline operator bool(){return b;}
+    }
+    inline operator bool(){return b;}
 }io;
